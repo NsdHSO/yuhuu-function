5e8b40fea677e8119b7cd781116cf89f6c6540c5
NsdHSO <nechiforelsamuel@gmail.com>
2026-02-14 05:01:41 +0200

    feat: 🎸 added routes for user-roles

--- crates/functions/src/features/user_roles/routes.rs
+++ crates/functions/src/features/user_roles/routes.rs
@@ -5,21 +5,11 @@ use super::handlers;
 /// Configure user_roles routes
 /// These routes are integrated into users and roles scopes
 pub fn configure_user_roles(cfg: &mut web::ServiceConfig) {
-    // Routes for managing user roles
-    cfg.route(
-        "/users/{user_id}/roles",
-        web::get().to(handlers::get_user_roles),
-    )
-    .route(
-        "/users/{user_id}/roles",
-        web::post().to(handlers::assign_role),
-    )
-    .route(
-        "/users/{user_id}/roles/{role_id}",
-        web::delete().to(handlers::remove_role),
-    )
-    .route(
-        "/roles/{role_id}/users",
-        web::get().to(handlers::get_users_by_role),
+    (*cfg).service(
+        web::resource("/users/{user_id}/roles")
+            .route(web::get().to(handlers::get_user_roles))
+            .route(web::post().to(handlers::assign_role))
+            .route(web::delete().to(handlers::remove_role))
+            .route(web::get().to(handlers::get_users_by_role)),
     );
 }
